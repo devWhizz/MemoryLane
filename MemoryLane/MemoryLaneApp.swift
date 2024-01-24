29c277bc75aebeed6fb76d99ba58fd81d507ca35
devWhizz <martin.roensch@gmail.com>
2024-01-24 09:10:18 +0100

    Initial Commit

---
+++ MemoryLane/MemoryLaneApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryLaneApp.swift
+//  MemoryLane
+//
+//  Created by syntax on 24.01.24.
+//
+
+import SwiftUI
+
+@main
+struct MemoryLaneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
